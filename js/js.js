00b5ecf61963b2b02429d8599ae22b62d7e51e84
diana chuctaya pacheco <leydiana.ch2015@gmail.com>
2017-07-26 14:21:46 -0500

    ultimo

--- js/js.js
+++ js/js.js
@@ -1,21 +1,4 @@
-    var Notas=new Array(20);
-    var N =1;
-    var nuevalistaOuput =document.getElementById('nuevalista');
-    var newlista=document.getElementById('mas-listas').value;
-     for(var i=0;i<N;i++){
-         Notas[i]=undefined;
-     }
- 
-    var addnota=document.getElementById('add');
-    addnota.onclick=function () {
-        var newlista=document.getElementById('mas-listas').value;
-         console.log(newlista)
-         
-        Notas.end(newlista);
-         document.getElementById('nuevalista').innerHTML="<br>"+ Notas +'<br>';
-    }
-     console.log(newlista)
-     console.log(Notas)      
+
         var arrayDatos=[
     {
         "userId": 1,
@@ -82,17 +65,30 @@
     for (var i =0; i<arrayDatos.length;i++){
 
         var dato_1 = arrayDatos[i];
-        // //alert (dato_1);
-        
-        // for (var html =0; html<arrayDatos.length;html++){
+     
         var s = "<div>" + 
             "<h3>" +  "List: " + dato_1.title + "</h3>" +"<h3>"+ "Number: " + dato_1.id + "</h3>"
         "</div>";
-        // document.write(s);
+        
      
         html += s; 
         document.getElementById('mostrar').innerHTML=html;
-        }
+        
+    }
+    function añadir() {
+    // // var array=[];
+    // var nuevalista=' ';
+    var nota=document.getElementById('mas-listas').Value
+    // nuevalista.push(nota);
+    
+    // for(i in array){
+    //     nuevalista +=' . '+array[i].slice(0,1);
+    // }
+    // document.getElementById('nuevalista'),innerHTML = nuevalista;
+    // nota = '';
+    arrayDatos.push(nota);
+        //  document.getElementById('nuevalista').innerHTML="<br>"+ Notas +'<br>';
+}     
 
     
     
